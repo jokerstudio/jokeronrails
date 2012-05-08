7691166ffcbf1068429e076157ce5f8e0e9ca822
jokerstudio <jokerstudio26@gmail.com>
2012-05-08 15:36:19 +0700

    chang db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120503022706) do
+ActiveRecord::Schema.define(:version => 20120508083319) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
