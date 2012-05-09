fbddafd3cacea0228b29948f2801f22772aa5b99
jokerstudio <jokerstudio26@gmail.com>
2012-05-09 18:04:42 +0700

    add devise to heroku

--- db/migrate/20120509071221_devise_create_users.rb
+++ db/migrate/20120509071221_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
       t.string :name, default: ''
+      t.boolean :admin, default: false
 
       ## Recoverable
       t.string   :reset_password_token
