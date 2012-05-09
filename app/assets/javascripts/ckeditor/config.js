65160b28653ae97064f7cddb40c01f81eb40d9f2
jokerstudio <jokerstudio26@gmail.com>
2012-05-09 20:14:03 +0700

    add ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -5,6 +5,48 @@
 
 CKEDITOR.editorConfig = function( config )
 {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+    
+  /* Filebrowser routes */
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
+  config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Flash dialog.
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+
+  // The location of a script that handles file uploads in the Flash dialog.
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+  
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+
+  // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+
+  // The location of a script that handles file uploads in the Image dialog.
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  
+  // The location of a script that handles file uploads.
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
+  
+  // Rails CSRF token
+  config.filebrowserParams = function(){
+    var csrf_token = jQuery('meta[name=csrf-token]').attr('content'),
+        csrf_param = jQuery('meta[name=csrf-param]').attr('content'),
+        params = new Object();
+    
+    if (csrf_param !== undefined && csrf_token !== undefined) {
+      params[csrf_param] = csrf_token;
+    }
+    
+    return params;
+  };
+  
+  /* Extra plugins */
+  // works only with en, ru, uk locales
+  config.extraPlugins = "embed,attachment";
     config.toolbar = 'Easy';
 
     config.toolbar_Easy =
@@ -12,7 +54,7 @@ CKEDITOR.editorConfig = function( config )
             ['Bold','Italic','Strike'],
             ['BulletedList','NumberedList','Blockquote'],
             ['JustifyLeft','JustifyCenter','JustifyRight'],
-            ['Link','Unlink'],
+            ['Image','Link','Unlink'],
             ['Subscript', 'Superscript'],
             ['YahooMaps', 'Dictionary'],
             '/',
