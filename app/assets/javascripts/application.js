25642a50430b881240a476bdbbdbf66e2ad6c28c
jokerstudio <jokerstudio26@gmail.com>
2012-05-09 21:27:04 +0700

    new ckeditor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ckeditor/init
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-alert
-//= require_tree .
+//= require ckeditor/ckeditor.js
 
