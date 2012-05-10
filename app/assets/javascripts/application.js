29043add9cc254bcfe441dbc644eb909f9725708
jokerstudio <jokerstudio26@gmail.com>
2012-05-10 11:12:34 +0700

    ckeditor o

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ckeditor/init
+//= require ckeditor/init.js
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-alert
-//= require_tree .
 
 
 
