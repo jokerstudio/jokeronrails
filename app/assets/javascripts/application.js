812baddf7a15ee66c52b928663f4f2011f70f823
jokerstudio <jokerstudio26@gmail.com>
2012-05-10 09:12:31 +0700

    joker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
+var CKEDITOR_BASEPATH = '/assets/javascripts/ckeditor/';
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-alert
