e7973391a6e10a39833e3512943a4c42da493754
jokerstudio <jokerstudio26@gmail.com>
2012-05-10 08:38:27 +0700

    java precompile

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Bloggy
     # Enable the asset pipeline
     config.assets.enabled = true
     #config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['javascripts/ckeditor/*'] 
+
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
